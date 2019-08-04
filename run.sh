26c3cf3f6d9888516e19872e1f4286f2dfe89c7b
chenee <chenee543216@gmail.com>
2019-08-04 10:24:49 +0800

    refine

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/chenee/ssr/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
 ./shadowsocks-all.sh 2>&1 | tee shadowsocks-all.log
-wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
+wget --no-check-certificate https://raw.githubusercontent.com/chenee/ssr/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 
